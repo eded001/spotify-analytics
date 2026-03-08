bb5451f9f04aa146bb1503e22494f48a53e7a960
Ed <108343328+eded001@users.noreply.github.com>
2026-03-08 12:25:03 -0300

    feat: add content

--- app/src/content/Footer.tsx
+++ app/src/content/Footer.tsx
@@ -1,7 +1,25 @@
+import Anchor from "@/components/Anchor";
+import List from "@/components/List";
+import Link from "next/dist/client/link";
+
 export default function Footer() {
     return (
-        <footer className="w-full text-center text-gray-400 position-fixed bottom-0 bg-red-500">
-            <p>Made by <a href="https://eded001.github.io/portfolio/" target="_blank" rel="noopener noreferrer" className="text-[#1db954] hover:underline">Edgar Augusto</a></p>
+        <footer className="w-full text-center text-gray-3001 pb-5">
+            <nav className="md:hidden flex justify-center">
+                <List
+                    className="flex gap-2 text-sm font-medium"
+                    separator={<span className="text-zinc-500">|</span>}
+                    items={[
+                        <Link href="/" key="Home" className="text-[#1db954] hover:underline">Home</Link>,
+                        <Link href="/about" key="About" className="text-[#1db954] hover:underline">About</Link>,
+                        <Link href="/privacy-policy" key="Privacy Policy" className="text-[#1db954] hover:underline">Privacy Policy</Link>,
+                        <Link href="/contact" key="Contact" className="text-[#1db954] hover:underline">Contact</Link>,
+                        <Link href="/faq" key="FAQ" className="text-[#1db954] hover:underline">FAQ</Link>
+                    ]}
+                />
+            </nav>
+
+            <p className="text-[#e8f8ee] pt-5">Made by <Anchor href="https://eded001.github.io/portfolio/" target="_blank">Edgar Augusto</Anchor></p>
         </footer>
     );
 }
--- app/src/content/Header.tsx
+++ app/src/content/Header.tsx
@@ -1,26 +1,22 @@
+import Anchor from "@/components/Anchor";
+import List from "@/components/List";
+
 export default function Header() {
     return (
-        <header className="w-full flex items-center justify-between p-5">
-            <div>
-                <h1 className="text-3xl text-[#1db954] font-bold">Spolytics</h1>
-                <p className="text-sm text-gray-400">Your Spotify Analytics Dashboard</p>
-            </div>
+        <header className="flex justify-center md:justify-between items-center bg-[#141010] p-5">
+            <h1 className="text-4xl font-bold text-[#1db954]">Spotify Analytics</h1>
 
-            <nav>
-                <ul className="flex gap-8 text-sm text-gray-300 font-medium">
-                    <li className="hover:text-[#1db954] cursor-pointer transition">
-                        Home
-                    </li>
-                    <li className="hover:text-[#1db954] cursor-pointer transition">
-                        About
-                    </li>
-                    <li className="hover:text-[#1db954] cursor-pointer transition">
-                        Privacy Policy
-                    </li>
-                    <li className="hover:text-[#1db954] cursor-pointer transition">
-                        Contact
-                    </li>
-                </ul>
+            <nav className="hidden md:block">
+                <List
+                    className="flex gap-8 font-medium"
+                    items={[
+                        <Anchor href="/" key="Home">Home</Anchor>,
+                        <Anchor href="/about" key="About">Sobre</Anchor>,
+                        <Anchor href="/privacy-policy" key="Privacy Policy">Política de Privacidade</Anchor>,
+                        <Anchor href="/contact" key="Contact">Contato</Anchor>,
+                        <Anchor href="/faq" key="FAQ">Dúvidas</Anchor>
+                    ]}
+                />
             </nav>
         </header>
     );
