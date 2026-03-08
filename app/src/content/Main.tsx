ca94b047fb88d0d1249fcb191deb7f529f9d0fcb
Ed <108343328+eded001@users.noreply.github.com>
2026-03-08 12:25:13 -0300

    chore: remove a main file

--- app/src/content/Main.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <main className="min-h-screen bg-green-500 text-center">
-            <h1 className="text-4xl text-[#1db954] font-bold">Welcome to the Spolytics!</h1>
-            <h2 className="text-2xl text-[#27ae60] font-semibold">Track your music journey</h2>
-        </main>
-    );
-}
